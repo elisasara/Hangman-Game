dab9c4b61d7b352736039c32eef934b9266dae03
Elisa Urda <urda.elisa@gmail.com>
2018-02-22 12:30:00 -0500

    fixed issue with resetting guessesremain, finished readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Hangman-Game
+
+## Welcome to Hangman in the Jungle!
+
+**Instructions:**
+* Press a letter to start playing the game
+* If you guess a correct letter it will appear where it belongs in the word
+* If you guess an incorrect letter you will lose one of your remaining guesses
+    * Incorrect letters guessed will be listed under letters already guessed
+* You have a total of 12 incorrect guesses before you lose
+* Click the NEW WORD button to start a new game (your win count will continue counting as long as you do not refresh the page)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,15 +28,15 @@ function reset() {
     document.getElementById("guessesleft").innerHTML = guessesremain;
 
     // choose a new random word from the array
-var randomword = wordbank[Math.floor(Math.random() * wordbank.length)];
-console.log(randomword);
+    var randomword = wordbank[Math.floor(Math.random() * wordbank.length)];
+    console.log(randomword);
 
     // display the new word as dashes in the guesstheword div
-for (i = 0; i < randomword.length; i++) {
-    chosenword[i] = "_";
-    var combine = chosenword.join(" ");
-    document.getElementById("guesstheword").innerHTML = combine;
-}
+    for (i = 0; i < randomword.length; i++) {
+        chosenword[i] = "_";
+        var combine = chosenword.join(" ");
+        document.getElementById("guesstheword").innerHTML = combine;
+    }
 
 }
 
@@ -56,77 +56,81 @@ function start() {
     }
 }
 
-    document.onkeyup = function playgame() {
+document.onkeyup = function playgame() {
 
-        // onkeyup take the key that is chosen, make it lowercase
-        var playerguess = event.key.toLowerCase();
-        console.log(playerguess);
+    // onkeyup take the key that is chosen, make it lowercase
+    var playerguess = event.key.toLowerCase();
+    console.log(playerguess);
 
-        // determine if that key is a letter and if it has already been guessed.
-        if (alphabet.indexOf(playerguess) !== -1 && lettersguessed.indexOf(playerguess) === -1) {
+    // determine if that key is a letter and if it has already been guessed.
+    if (alphabet.indexOf(playerguess) !== -1 && lettersguessed.indexOf(playerguess) === -1) {
 
-            // if it is a letter and has not yet been guessed the lower the number of guesses remaining by one.
-            guessesremain--;
-            document.getElementById("guessesleft").innerHTML = guessesremain;
+        // if it is a letter and has not yet been guessed the lower the number of guesses remaining by one.
+        guessesremain--;
+        document.getElementById("guessesleft").innerHTML = guessesremain;
 
-            // add that letter to the array of lettersguessed
-            lettersguessed.push(playerguess);
-            console.log(lettersguessed);
+        // add that letter to the array of lettersguessed
+        lettersguessed.push(playerguess);
+        console.log(lettersguessed);
 
-            //add to the html of the div letters used so the user can see what letters they have guessed so far.
-            document.getElementById("lettersused").innerHTML += playerguess + " ";    
+        //add to the html of the div letters used so the user can see what letters they have guessed so far.
+        document.getElementById("lettersused").innerHTML += playerguess + " ";
+    }
+
+    // if playerguess is in the word, replace the word in the array to include the letter where it should be (replacing the dash that was there)
+    for (i = 0; i < randomword.length; i++) {
+
+        if (playerguess === randomword[i]) {
+
+            chosenword[i] = playerguess;
+            console.log(chosenword[i]);
+            var combine = chosenword.join(" ");
+            document.getElementById("guesstheword").innerHTML = combine;
+        }
+    }
+
+    // convert the chosenword array into a string to determine if the player wins.
+    var chosenwordstring = chosenword.toString();
+
+    // if the string does not consist of any dashes do the following
+    if (chosenword.indexOf("_") === -1) {
+
+        //increase the wincount by 1
+        wincount++;
+        document.getElementById("wins").innerHTML = wincount;
+
+        // display the appropriate photo based on the word
+        if (randomword === "tiger") {
+            document.getElementById("wordimage").innerHTML = "<img src='assets/images/tiger.jpg'>";
+        }
+        else if (randomword === "monkey") {
+            document.getElementById("wordimage").innerHTML = "<img src='assets/images/monkey.jpg'>";
+        }
+        else if (randomword === "leopard") {
+            document.getElementById("wordimage").innerHTML = "<img src='assets/images/leopard.jpg'>";
+        }
+        else if (randomword === "gorilla") {
+            document.getElementById("wordimage").innerHTML = "<img src='assets/images/gorilla.jpg'>";
+        }
+        else if (randomword === "elephant") {
+            document.getElementById("wordimage").innerHTML = "<img src='assets/images/elephant.jpg'>";
+        }
+        else if (randomword === "anaconda") {
+            document.getElementById("wordimage").innerHTML = "<img src='assets/images/anaconda.jpg'>";
         }
 
-        // if playerguess is in the word, replace the word in the array to include the letter where it should be (replacing the dash that was there)
-        for (i = 0; i < randomword.length; i++) {
-
-            if (playerguess === randomword[i]) {
-
-                chosenword[i] = playerguess;
-                console.log(chosenword[i]);
-                var combine = chosenword.join(" ");
-                document.getElementById("guesstheword").innerHTML = combine;
-            }
-        }
-
- // convert the chosenword array into a string to determine if the player wins.
-  var chosenwordstring = chosenword.toString();
-
-  // if the string does not consist of any dashes do the following
-  if (chosenword.indexOf("_") === -1) {
-
-      //increase the wincount by 1
-      wincount++;
-    document.getElementById("wins").innerHTML = wincount;
-    
-    // display the appropriate photo based on the word
-    if (randomword === "tiger") {
-        document.getElementById("wordimage").innerHTML = "<img src='assets/images/tiger.jpg'>";
+        //reset guesses remain to 12
+        guessesremain = 12;
     }
-    else if (randomword === "monkey") {
-        document.getElementById("wordimage").innerHTML = "<img src='assets/images/monkey.jpg'>";
-    }
-    else if (randomword === "leopard") {
-        document.getElementById("wordimage").innerHTML = "<img src='assets/images/leopard.jpg'>";
-    }
-    else if (randomword === "gorilla") {
-        document.getElementById("wordimage").innerHTML = "<img src='assets/images/gorilla.jpg'>";
-    }
-    else if (randomword === "elephant") {
-        document.getElementById("wordimage").innerHTML = "<img src='assets/images/elephant.jpg'>";
-    }
-    else if (randomword === "anaconda") {
-        document.getElementById("wordimage").innerHTML = "<img src='assets/images/anaconda.jpg'>";
-    }
-  }
 
-// if the player runs out of guesses
-if (guessesremain === 0) {
+    // if the player runs out of guesses
+    if (guessesremain === 0) {
 
-    //show an alert directing them to hit the "new word" button
-    alert("Sorry, you lost! Click the New Word button to try again.");
+        //show an alert directing them to hit the "new word" button
+        alert("Sorry, you lost! Click the New Word button to try again.");
+
+        //reset guessesremain to 12
+        guessesremain = 12;
+    }
 }
-    }
-    
 
-// set rule for no more guesses after game is won or lost until button clicked
